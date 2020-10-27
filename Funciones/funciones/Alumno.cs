a9d310420e9c39c58ce8d0a3ebd56cd7f1eddba3
Dany Galan <danygalan1334@gmail.com>
2020-10-26 21:36:34 -0600

    Ejercicio Funciones Void

--- Funciones/funciones/Alumno.cs
+++ Funciones/funciones/Alumno.cs
@@ -5,15 +5,29 @@ public class Alumno
     public string PrimerNombre { get ; set; }
 
     public string SegundoNombre { get ; set; }
+
+    public bool Activo { get; set; }
+
+
     public Alumno(int id, string primerNombre, string segundoNombre)
     {
         Id = id;
         PrimerNombre = primerNombre;
         SegundoNombre = segundoNombre;
+        Activo = true;
     }
     public string nombreCompleto(){
-        return PrimerNombre + " " + SegundoNombre;
+        if (Activo == true){
+            return PrimerNombre + " " + SegundoNombre;    
+        }
         
+        return "Alumno Inactivo";
+    }
+    public void inactivarAlumno(){
+        Activo = false;
     }
 
+    public void activarAlumno(){
+        Activo = true;
+    }
 }
--- Funciones/funciones/Program.cs
+++ Funciones/funciones/Program.cs
@@ -7,8 +7,19 @@ namespace funciones
         static void Main(string[] args)
         {
             Alumno a = new Alumno(1, "Juan", "Perez");
+            Alumno b = new Alumno(2, "Maria", "MArtinez");
             
+            a.inactivarAlumno();
+
             Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+
+            Console.WriteLine("");
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
         }
     }
 }
