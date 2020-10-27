25f7a04a15480c94ada775200e4739cd9e6a1faf
Dany Galan <danygalan1334@gmail.com>
2020-10-26 18:04:39 -0600

    Ejercicios propiedades

---
+++ propiedades/Alumno.cs
@@ -0,0 +1,13 @@
+public class Alumno
+{
+    public int id { get; set; }
+
+    public string PrimerNombre { get; set; }
+
+    public string SegundoNombre { get; set; }
+
+    public Alumno(){
+        
+    }
+
+}
---
+++ propiedades/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a.id = 1;
+            a.PrimerNombre = "Juan";
+            a.SegundoNombre = "Perez";
+            
+            Alumno b = new Alumno();
+            b.id = 2;
+            b.PrimerNombre = "Maria";
+            b.SegundoNombre = "Martinez";
+
+            Console.WriteLine(a.PrimerNombre);
+        }
+    }
+}
